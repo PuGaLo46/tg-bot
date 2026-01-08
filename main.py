adfb5ef0418d79d572be2803d5bae41722afe998
PuGaLo46 <operkurska@gmail.com>
2026-01-08 22:02:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import os
-from collections import defaultdict, deque
-from telegram import Update
-from telegram.ext import (
-    ApplicationBuilder, CommandHandler, MessageHandler,
-    ContextTypes, filters
-)
 from openai import OpenAI
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
 client = OpenAI(api_key=OPENAI_API_KEY)
